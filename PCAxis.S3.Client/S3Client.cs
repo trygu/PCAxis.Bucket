ea63385a0816f3bdf8d5b6ee973a48d7b8c75f2f
Trygve Falch <trygve.falch@gmail.com>
2023-08-18 13:34:37 +0200

    Added documentation to the class.

--- PCAxis.S3.Client/S3Client.cs
+++ PCAxis.S3.Client/S3Client.cs
@@ -9,11 +9,23 @@ using Amazon.S3.Model;
 
 namespace PCAxis.S3.Client
 {
+    /// <summary>
+    /// Provides functionality for interacting with S3 (or S3-compatible) services.
+    /// </summary>
     public class S3Client
     {
         internal readonly IAmazonS3 client;
         private readonly string? defaultBucketName;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="S3Client"/> class.
+        /// </summary>
+        /// <param name="endpoint">The endpoint URL of the S3 service.</param>
+        /// <param name="accessKey">The access key for the S3 service.</param>
+        /// <param name="secretKey">The secret key for the S3 service.</param>
+        /// <param name="useGCS">If set to true, will use Google Cloud Storage's signature version V4. Defaults to false.</param>
+        /// <param name="defaultBucketName">The default bucket name to use if none is specified in methods. Defaults to null.</param>
+        /// <param name="amazonS3">An optional custom S3 client instance. Defaults to null.</param>
         public S3Client(string endpoint, string accessKey, string secretKey, bool useGCS = false, string? defaultBucketName = null, IAmazonS3? amazonS3 = null)
         {
             var clientConfig = new AmazonS3Config
@@ -32,6 +44,10 @@ namespace PCAxis.S3.Client
             this.defaultBucketName = defaultBucketName;
         }
 
+        /// <summary>
+        /// Creates a new bucket.
+        /// </summary>
+        /// <param name="bucketName">The name of the bucket to create. If not provided, uses the default bucket name.</param>
         public void CreateBucket(string? bucketName = null)
         {
             var bucket = bucketName ?? defaultBucketName;
@@ -42,6 +58,11 @@ namespace PCAxis.S3.Client
             client.PutBucketAsync(request).Wait();
         }
 
+        /// <summary>
+        /// Lists all files in a bucket.
+        /// </summary>
+        /// <param name="bucketName">The name of the bucket to list files from. If not provided, uses the default bucket name.</param>
+        /// <returns>A list of <see cref="S3Object"/> representing the files in the bucket.</returns>
         public List<S3Object> ListFiles(string? bucketName = null)
         {
             var bucket = bucketName ?? defaultBucketName;
@@ -62,6 +83,12 @@ namespace PCAxis.S3.Client
             return allFiles;
         }
 
+        /// <summary>
+        /// Uploads a file to a bucket.
+        /// </summary>
+        /// <param name="key">The key (path) where the file should be stored in the bucket.</param>
+        /// <param name="fileStream">The stream containing the file content to upload.</param>
+        /// <param name="bucketName">The name of the bucket to upload to. If not provided, uses the default bucket name.</param>
         public void UploadFile(string key, Stream fileStream, string? bucketName = null)
         {
             var bucket = bucketName ?? defaultBucketName;
@@ -74,6 +101,11 @@ namespace PCAxis.S3.Client
             client.PutObjectAsync(request).Wait();
         }
 
+        /// <summary>
+        /// Deletes a file from a bucket.
+        /// </summary>
+        /// <param name="key">The key (path) of the file to delete.</param>
+        /// <param name="bucketName">The name of the bucket to delete from. If not provided, uses the default bucket name.</param>
         public void DeleteFile(string key, string? bucketName = null)
         {
             var bucket = bucketName ?? defaultBucketName;
@@ -85,6 +117,10 @@ namespace PCAxis.S3.Client
             client.DeleteObjectAsync(request).Wait();
         }
 
+        /// <summary>
+        /// Deletes a bucket.
+        /// </summary>
+        /// <param name="bucketName">The name of the bucket to delete. If not provided, uses the default bucket name.</param>
         public void DeleteBucket(string? bucketName = null)
         {
             var bucket = bucketName ?? defaultBucketName;
@@ -95,6 +131,12 @@ namespace PCAxis.S3.Client
             client.DeleteBucketAsync(request).Wait();
         }
 
+        /// <summary>
+        /// Reads a file from a bucket and processes its content using a provided action.
+        /// </summary>
+        /// <param name="key">The key (path) of the file to read.</param>
+        /// <param name="processStream">The action to execute on the file's content stream.</param>
+        /// <param name="bucketName">The name of the bucket to read from. If not provided, uses the default bucket name.</param>
         public void ReadFile(string key, Action<Stream> processStream, string? bucketName = null)
         {
             var bucket = bucketName ?? defaultBucketName;
